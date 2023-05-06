ab19021df54db258eb3965b3ea054045024c5e33
Damir Modyarov <otomir@yandex.ru>
2023-05-06 17:35:39 +0300

    Improve team gallery UX using physics

--- src/app/[locale]/_components/drag-slider.tsx
+++ src/app/[locale]/_components/drag-slider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {ReactNode, useCallback, useRef, useState} from "react";
+import {ReactNode, useCallback, useEffect, useRef, useState} from "react";
 
 export type DragSliderProps = {
     children: ReactNode;
@@ -14,6 +14,9 @@ export default function DragSlider({children, buttonHeight}: DragSliderProps) {
     const [dragged, setDragged] = useState(false);
     const root = useRef<HTMLDivElement>(null);
     const currentTouch = useRef<{ x: number, y: number } | null>(null);
+    const velX = useRef(0);
+    const accX = useRef(0);
+    const decelerationCoefficient = useRef(0.4);
 
     const startDrag = useCallback((pageY: number) => {
         if (!root.current) return;
@@ -27,15 +30,34 @@ export default function DragSlider({children, buttonHeight}: DragSliderProps) {
     }, [])
     const updateDrag = useCallback((movX: number) => {
         if (!dragged || !root.current) return;
-        root.current.scrollLeft -= movX;
+        accX.current = -movX;
+    }, [dragged])
+
+    useEffect(() => {
+        let currentFrame = 0;
+        const update = () => {
+            if (root.current) {
+                velX.current += accX.current;
+                root.current.scrollLeft += velX.current;
+                accX.current = 0;
+                velX.current *= decelerationCoefficient.current;
+            }
+            currentFrame = requestAnimationFrame(update);
+        }
+        currentFrame = requestAnimationFrame(update);
+        return () => cancelAnimationFrame(currentFrame);
     }, [dragged])
 
     return (
         <div
             className={`grid grid-flow-col select-none gap-8 items-center w-full max-w-full overflow-x-hidden ${dragged ? 'cursor-grabbing' : 'cursor-grab'}`}
-            onMouseDown={e => startDrag(e.pageY)}
+            onMouseDown={e => {
+                startDrag(e.pageY)
+                decelerationCoefficient.current = 0.4;
+            }}
             onTouchStart={e => {
                 startDrag(e.touches[0].pageY)
+                decelerationCoefficient.current = 0.9;
             }}
 
             onMouseUp={stopDrag}
