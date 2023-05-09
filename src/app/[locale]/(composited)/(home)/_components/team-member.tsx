b4b2a4df4653af5085f849ca16de269c3703097f
Damir Modyarov <otomir@yandex.ru>
2023-05-09 13:03:03 +0300

    Improve mobile team member card UX

--- src/app/[locale]/(composited)/(home)/_components/team-member.tsx
+++ src/app/[locale]/(composited)/(home)/_components/team-member.tsx
@@ -12,18 +12,18 @@ export type TeamMemberProps = {
 export default function TeamMember({href, name, role, img, buttonText}: TeamMemberProps) {
     return (
         <figure
-            className="group border-2 border-neutral-300 rounded-4xl p-8 flex flex-col justify-center duration-300 ease-in-out
+            className="group border-2 border-neutral-300 rounded-4xl p-4 md:p-8 flex flex-col justify-center duration-300 ease-in-out
             items-center w-72 md:w-96 h-96 md:h-[32rem] hover:border-primary-500 overflow-hidden bg-white transition-colors"
         >
             <div className="w-full flex-1" />
-            <Image src={img} alt={name} className="rounded-full aspect-square mb-6" width={150} height={150} />
+            <Image src={img} alt={name} className="rounded-full aspect-square mb-6 w-24 md:w-36" width={150} height={150} />
             <h6 className="font-display font-bold text-2xl md:text-3xl">{name}</h6>
             <p className="text-xl md:text-2xl">{role}</p>
             <div className="w-full flex-1" />
             <Link
-                className="translate-y-28 group-hover:translate-y-0 transition-transform w-full py-3 md:py-6 text-xl md:text-2xl
-                bg-primary-500 text-white rounded-xl active:bg-primary-400 active:text-white flex justify-center items-center
-                duration-300 ease-in-out"
+                className="can-hover:translate-y-28 group-hover:translate-y-0 transition-transform w-full py-4 md:py-6 text-xl
+                md:text-2xl bg-primary-500 text-white rounded-xl active:bg-primary-400 active:text-white flex justify-center
+                items-center duration-300 ease-in-out"
                 href={href}
                 target="_blank"
             >
