346132bc1f6232cd282853078c4361a5cbf39efb
Damir Modyarov <otomir@yandex.ru>
2023-05-10 19:20:37 +0300

    Fix SVG loading

--- src/loaders/svg.ts
+++ src/loaders/svg.ts
@@ -1,8 +1,10 @@
 import "server-only";
-import fs from "fs";
+import fs from 'fs'
+import path from 'path'
 import {createElement} from "react";
 
-export function unsafelyLoadSVG(path: string) {
-    const contents = fs.readFileSync('./public/' + path, "utf-8");
+export function unsafelyLoadSVG(filePath: string) {
+    const absolutePath = path.resolve('./public', ...filePath.split('/').filter(x => x !== ''));
+    const contents = fs.readFileSync(absolutePath, "utf-8");
     return createElement("span", {dangerouslySetInnerHTML: {__html: contents}});
 }
