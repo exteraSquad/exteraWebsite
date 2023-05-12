1a71e55f4c3e7cbf350af7de0d15505ea4ee9a53
Damir Modyarov <otomir@yandex.ru>
2023-05-12 23:36:50 +0300

    Add team member background

--- src/app/[locale]/(composited)/(home)/_components/team-member.tsx
+++ src/app/[locale]/(composited)/(home)/_components/team-member.tsx
@@ -13,8 +13,15 @@ export default function TeamMember({href, name, role, img, buttonText}: TeamMemb
     return (
         <figure
             className="group border-2 border-neutral-300 rounded-4xl p-4 md:p-8 flex flex-col justify-center duration-300 ease-in-out
-            items-center w-72 md:w-96 h-96 md:h-[32rem] hover:border-primary-500 overflow-hidden bg-white transition-colors"
+            items-center w-72 md:w-96 h-96 md:h-[32rem] hover:border-primary-500 overflow-hidden transition-colors relative"
         >
+            <Image
+                src="/images/team/background.png"
+                alt=""
+                className="absolute inset-0 w-full h-full object-cover -z-10 opacity-0 group-hover:opacity-100 duration-300 ease-in-out"
+                width={380}
+                height={507}
+            />
             <div className="w-full flex-1" />
             <Image src={img} alt={name} className="rounded-full aspect-square mb-6 w-24 md:w-36" width={150} height={150} />
             <h6 className="font-display font-bold text-2xl md:text-3xl">{name}</h6>
