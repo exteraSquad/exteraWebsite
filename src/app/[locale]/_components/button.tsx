231db4ac8c86801e3b4c38001e61dad2fd294930
Damir Modyarov <otomir@yandex.ru>
2023-05-13 12:12:51 +0300

    Animate button scale

--- src/app/[locale]/_components/button.tsx
+++ src/app/[locale]/_components/button.tsx
@@ -10,8 +10,9 @@ export default function Button({className, children, onClick, disabled}: ButtonP
     return (
         <button
             className={`rounded-full font-display font-bold text-2xl text-primary-500 hover:text-white py-4 px-8
-                hover:bg-primary-500 bg-primary-100 transition-colors active:bg-primary-400 duration-300 ease-in-out 
-                active:text-white disabled:opacity-50 disabled:pointer-events-none ${className || ""}`}
+                hover:bg-primary-500 bg-primary-100 active:bg-primary-400 duration-300 ease-in-out transition-all
+                active:text-white disabled:opacity-50 disabled:pointer-events-none hover:scale-105 active:scale-95
+                ${className || ""}`}
             onClick={onClick}
             disabled={disabled}
         >
