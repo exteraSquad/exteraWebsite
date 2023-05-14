3b00b946490bded18eaed33678adc18d3c2f8113
Damir Modyarov <otomir@yandex.ru>
2023-05-14 11:11:18 +0300

    Increase top section margin

--- src/app/[locale]/(composited)/(home)/page.tsx
+++ src/app/[locale]/(composited)/(home)/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <>
             <IconCanvas probability={0.02} paths={checkedIcons} color="#F54142" height={1700}/>
 
-            <section className="w-full flex justify-center items-center flex-col gap-4 mt-32 md:mt-48 mb-16 px-4" id="top">
+            <section className="w-full flex justify-center items-center flex-col gap-4 mt-48 md:mt-72 mb-16 px-4" id="top">
                 <h1 className="text-primary-500 font-bold font-display text-5xl md:text-6xl lg:text-7xl">{t("title")}</h1>
                 <p className="text-xl md:text-2xl text-neutral-800 w-full md:w-[30rem] text-center">{t("description")}</p>
                 <Link
