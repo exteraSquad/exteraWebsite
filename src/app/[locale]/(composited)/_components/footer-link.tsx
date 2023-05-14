5a2cffe32c168c590ab26e3a7c6470e67727bba0
Damir Modyarov <otomir@yandex.ru>
2023-05-14 11:27:47 +0300

    Fix overflow issue

--- src/app/[locale]/(composited)/_components/footer-link.tsx
+++ src/app/[locale]/(composited)/_components/footer-link.tsx
@@ -9,7 +9,7 @@ export default function FooterLink({href, children}: FooterLinkProps) {
     return (
         <Link
             href={href}
-            className="text-neutral-800 hover:text-black lg:hover:font-semibold text-4xl md:text-3xl lg:text-lg transition-all
+            className="text-neutral-800 hover:text-black lg:hover:font-semibold text-3xl lg:text-lg transition-all
             w-fit text-center lg:text-left font-display font-bold lg:font-sans lg:font-normal"
             underline
             key={children}
