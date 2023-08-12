e588b2f81d3c0b448d1b00386d4279178aadf69b
Damir Modyarov <otomir@yandex.ru>
2023-08-12 15:37:18 +0300

    Improve 404 for mobiles

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -12,8 +12,8 @@ export default function ErrorNotFound() {
         <>
             <IconCanvas probability={0.02} paths={checkedIcons} color="#F54142" />
             <div className="flex flex-col items-center justify-center h-screen w-full gap-8 text-center">
-                <h1 className="font-display font-bold text-9xl text-primary-500">404</h1>
-                <p className="text-3xl max-w-xl">{t("description")}</p>
+                <h1 className="font-display font-bold text-8xl md:text-9xl text-primary-500">404</h1>
+                <p className="text-xl md:text-3xl px-4 max-w-md md:max-w-xl">{t("description")}</p>
                 <Link href="/" className="text-primary-500 font-bold text-3xl">
                     <Button>
                         {t("button")}
